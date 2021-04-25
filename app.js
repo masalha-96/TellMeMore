54d671fd59004cac656ccd125cc7ebfb3efc58c9
Masalha-96 <Smasalha96@gmail.com>
2021-04-25 15:04:03 +0300

    add html&js files

---
+++ app.js
@@ -0,0 +1,13 @@
+
+let  userName = prompt("Hello I'm doing a simple survey, this will be quick, lets start with your name?");
+let userAge = prompt('Welcome ' + userName + ' How old are you?');
+let userTall = prompt(userAge +' Cool!, How tall are you ' + userName+ '? in (m) please. ');
+let userWeight = prompt('How much do you weigh?');
+
+
+
+alert('Welcome To My Page Mr/Ms ' + userName + ' :: Your Age is ' + userAge + ' & Your Body Mass Index is ' + (( userWeight) / (userTall * userTall)) );
+
+
+//document.write('Welcome To My Page Mr/Ms ' + userName + ' Your Age is ' + userAge + ' Your Perfect Weight is ' + userResult);
+
